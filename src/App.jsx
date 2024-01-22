f4da49125ec398334453350c2bafbc90f2febee2
Fanny Bagus Ramadhan <fannybagus9f@gmail.com>
2024-01-22 13:33:20 +0700

    feat(temperature): add other temperature
    [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,104 @@ import {
 import { ConvertRangeRegular } from "@fluentui/react-icons";
 import "./App.css";
 import useConverter from "./utils/useConverter";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
-const option = [
+const options = [
   {
     id: 1,
-    label: "celcius to fahrenheit",
-    value: "celcius",
-    symbol: "c",
+    label: "Celsius to Fahrenheit",
+    value: "ctf",
+    symbol: "°C",
+    result: "°F",
   },
   {
     id: 2,
-    label: "fahrenheit to celcius",
-    value: "fahrenheit",
-    symbol: "f",
+    label: "Celsius to Kelvin",
+    value: "ctk",
+    symbol: "°C",
+    result: "°K",
+  },
+  {
+    id: 3,
+    label: "Celsius to Reamur",
+    value: "ctr",
+    symbol: "°C",
+    result: "Re",
+  },
+  {
+    id: 4,
+    label: "Fahrenheit to Celsius",
+    value: "ftc",
+    symbol: "°F",
+    result: "°C",
+  },
+  {
+    id: 5,
+    label: "Fahrenheit to Kelvin",
+    value: "ftk",
+    symbol: "°F",
+    result: "°K",
+  },
+  {
+    id: 6,
+    label: "Fahrenheit to Reamur",
+    value: "ftr",
+    symbol: "°F",
+    result: "Re",
+  },
+  {
+    id: 7,
+    label: "Kelvin to Celsius",
+    value: "ktc",
+    symbol: "K",
+    result: "°C",
+  },
+  {
+    id: 8,
+    label: "Kelvin to Fahrenheit",
+    value: "ktf",
+    symbol: "K",
+    result: "°F",
+  },
+  {
+    id: 9,
+    label: "Kelvin to Reamur",
+    value: "ktr",
+    symbol: "K",
+    result: "Re",
+  },
+  {
+    id: 10,
+    label: "Reamur to Celsius",
+    value: "rtc",
+    symbol: "Re",
+    result: "°C",
+  },
+  {
+    id: 11,
+    label: "Reamur to Fahrenheit",
+    value: "rtf",
+    symbol: "Re",
+    result: "°F",
+  },
+  {
+    id: 12,
+    label: "Reamur to Kelvin",
+    value: "rtk",
+    symbol: "Re",
+    result: "K",
   },
 ];
 
+
 export default function App() {
-  const [temperatureType, setTemperatureType] = useState("celcius");
+  const [temperatureType, setTemperatureType] = useState("ctf");
   const [temperatureInput, setTemperatureInput] = useState(0);
   const [temperature, handleTemperature] = useConverter(temperatureInput, temperatureType);
 
+  useEffect(() => {
+    handleTemperature();
+  }, [temperatureType, temperatureInput]);
   return (
     <FluentProvider theme={webLightTheme}>
       <Card style={{width: 400}}>
@@ -47,7 +123,7 @@ export default function App() {
           description={<Caption1>{import.meta.env.VITE_WEB_DESCRIPTION}</Caption1>}
         />
         <Select onChange={({ target }) => setTemperatureType(target.value)}>
-          {option.map((selection, index) => (
+          {options.map((selection, index) => (
             <option key={index} value={selection.value}>
               {selection.label}
             </option>
@@ -55,6 +131,7 @@ export default function App() {
         </Select>
         <Input
           placeholder="input suhu"
+          defaultValue={0}
           type="number"
           onChange={(e) => setTemperatureInput(e.target.value)}
         />
@@ -67,7 +144,7 @@ export default function App() {
             Convert
           </Button>
           <Input
-            value={`${temperature}° `}
+            value={`${temperature} ${options.find((option) => option.value === temperatureType).result}`}
             disabled
             className="input__result"
           />
--- src/utils/useConverter.js
+++ src/utils/useConverter.js
@@ -4,13 +4,98 @@ const FIXED_CALC = 32;
 export default function useConverter(temperatureInput, temperatureType) {
     const [temperature, setTemperature] = useState(0);
 
+    const input = parseFloat(temperatureInput);
     const handleTemperature = () => {
-        const FahrenheitToCelcius = ((temperatureInput - FIXED_CALC) * 5) / 9;
-        const CelciusToFahrenheit = temperatureInput * (9 / 5) + FIXED_CALC;
+        const fahrenheitToCelsius = (fahrenheit) => {
+            return (fahrenheit - 32) * (5/9);
+        };
 
-        temperatureType == "celcius"
-            ? setTemperature(CelciusToFahrenheit)
-            : setTemperature(FahrenheitToCelcius);
+        const fahrenheitToKelvin = (fahrenheit) => {
+            return (fahrenheit - 32) * (5/9) + 273.15;
+        };
+
+        const fahrenheitToReamur = (fahrenheit) => {
+            return (fahrenheit - 32) * (4/9);
+        };
+
+        const celsiusToFahrenheit = (celsius) => {
+            return (celsius * 9/5) + 32;
+        };
+
+        const celsiusToKelvin = (celsius) => {
+            return celsius + 273.15;
+        };
+
+        const celsiusToReamur = (celsius) => {
+            return celsius * (4/5);
+        };
+
+        const kelvinToCelsius = (kelvin) => {
+            return kelvin - 273.15;
+        };
+
+        const kelvinToFahrenheit = (kelvin) => {
+            return (kelvin - 273.15) * 9/5 + 32;
+        };
+
+        const kelvinToReamur = (kelvin) => {
+            return (kelvin - 273.15) * (4/5);
+        };
+
+        const reamurToCelsius = (reamur) => {
+            return reamur * (5/4);
+        };
+
+        const reamurToFahrenheit = (reamur) => {
+            return reamur * (9/4) + 32;
+        };
+
+        const reamurToKelvin = (reamur) => {
+            return reamur * (5/4) + 273.15;
+        };
+
+
+
+        if (temperatureType === "ctf") {
+            // Celsius to Fahrenheit
+            setTemperature(celsiusToFahrenheit(input))
+        } else if (temperatureType === "ctk") {
+            // Celsius to Kelvin
+            setTemperature(celsiusToKelvin(input))
+        } else if (temperatureType === "ctr") {
+            // Celsius to Reamur
+            setTemperature(celsiusToReamur(input))
+        } else if (temperatureType === "ftc") {
+            // Fahrenheit to Celsius
+            setTemperature(fahrenheitToCelsius(input))
+        } else if (temperatureType === "ftk") {
+            // Fahrenheit to Kelvin
+            setTemperature(fahrenheitToKelvin(input))
+        } else if (temperatureType === "ftr") {
+            // Fahrenheit to Reamur
+            setTemperature(fahrenheitToReamur(input))
+        } else if (temperatureType === "ktc") {
+            // Kelvin to Celsius
+            setTemperature(kelvinToCelsius(input))
+        } else if (temperatureType === "ktf") {
+            // Kelvin to Fahrenheit
+            setTemperature(kelvinToFahrenheit(input))
+        } else if (temperatureType === "ktr") {
+            // Kelvin to Reamur
+            setTemperature(kelvinToReamur(input))
+        } else if (temperatureType === "rtc") {
+            // Reamur to Celsius
+            setTemperature(reamurToCelsius(input))
+        } else if (temperatureType === "rtf") {
+            // Reamur to Fahrenheit
+            setTemperature(reamurToFahrenheit(input))
+        } else if (temperatureType === "rtk") {
+            // Reamur to Kelvin
+            setTemperature(reamurToKelvin(input))
+        } else {
+            console.log("Jenis konversi suhu tidak valid.");
+            return;
+        }
     };
     
     return [temperature, handleTemperature];
